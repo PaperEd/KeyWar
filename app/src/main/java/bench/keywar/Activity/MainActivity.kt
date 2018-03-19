99230d2fa1dbc42a312c04a097a78ea27e747152
이종현 <dwg7691@gmail.com>
2018-03-19 20:14:06 +0900

    initalize commit

---
+++ app/src/main/java/bench/keywar/Activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package bench.keywar.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import bench.keywar.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
